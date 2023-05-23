3affa4a33a42e7b488f4a65964ab7201f47c60d8
Tayni Schukes <taycschukes@gmail.com>
2023-05-23 13:12:40 -0300

    Ajuste

--- codes/registrando_usuario.php
+++ codes/registrando_usuario.php
@@ -6,169 +6,168 @@
     $sql2 = "SELECT * from usuario WHERE nome = '$nome'";
     $result = $conn->query($sql2);
 
-    if ($result->num_rows > 0) { ?>
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <title>Cadastro Confirmado</title>
-        <style>
-            main {
-                background-color: #76a490;
-                width: 400px;
-                height: 200px;
-                text-align: center;
-                margin-left: auto;
-                margin-right: auto;
-                border-radius: 20px;
-            
-            }
-            button {
-                background-color: #284b40;
-                color: #fff;
-                border-radius: 15px;
-                width: 150px;
-                height: 35px;
-                margin-top: 8%;
-                border: none;
-            }
-            button a{
-                text-decoration: none;
-                color: white;
-            }
-        </style>
-    </head>
-    <body>
-        <header>
-            <img class="logoclaro" src="../images/logoclaro.png" alt="Logo da empresa">
-            <nav>
-                <ul>
-                    <li><a href="../index.html">Home</a></li>
-                    <li><a href="plantas.php">Plantas</a></li>
-                    <li><a href="sensor.html">Sensores</a></li>
-                    <li><a href="calendario.html">Calendário</a></li>
-                    <li><a href="sobre.html">Sobre</a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <h1>Este usuário já foi cadastrado!</h1>
-            <button><a href="../index.html">Fazer login</a></button><br>
-        </main>
-    </body>
-    </html>
-    <?php } elseif ($conn->query($sql) === TRUE) {?>
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <title>Cadastro Confirmado</title>
-        <style>
-            main {
-                background-color: #76a490;
-                width: 400px;
-                height: 200px;
-                text-align: center;
-                margin-left: auto;
-                margin-right: auto;
-                border-radius: 20px;
-            
-            }
-            button {
-                background-color: #284b40;
-                color: #fff;
-                border-radius: 15px;
-                width: 150px;
-                height: 35px;
-                margin-top: 8%;
-                border: none;
-            }
-            button a{
-                text-decoration: none;
-                color: white;
-            }
-        </style>
-    </head>
-    <body>
-        <header>
-            <img class="logoclaro" src="../images/logoclaro.png" alt="Logo da empresa">
-            <nav>
-                <ul>
-                    <li><a href="../index.html">Home</a></li>
-                    <li><a href="plantas.php">Plantas</a></li>
-                    <li><a href="sensor.html">Sensores</a></li>
-                    <li><a href="calendario.html">Calendário</a></li>
-                    <li><a href="sobre.html">Sobre</a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <h1>Usuário Cadastrado!</h1>
-            <button><a href="../index.html">Voltar</a></button><br>
-        </main>
-    </body>
-    </html>
-<?php
-    } elseif ($conn->query($sql) === FALSE) { ?>
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <title>Cadastro Confirmado</title>
-        <style>
-            main {
-                background-color: #76a490;
-                width: 400px;
-                height: 200px;
-                text-align: center;
-                margin-left: auto;
-                margin-right: auto;
-                border-radius: 20px;
-            
-            }
-            button {
-                background-color: #284b40;
-                color: #fff;
-                border-radius: 15px;
-                width: 150px;
-                height: 35px;
-                margin-top: 8%;
-                border: none;
-            }
-            button a{
-                text-decoration: none;
-                color: white;
-            }
-        </style>
-    </head>
-    <body>
-        <header>
-            <img class="logoclaro" src="../images/logoclaro.png" alt="Logo da empresa">
-            <nav>
-                <ul>
-                    <li><a href="../index.html">Home</a></li>
-                    <li><a href="plantas.php">Plantas</a></li>
-                    <li><a href="sensor.html">Sensores</a></li>
-                    <li><a href="calendario.html">Calendário</a></li>
-                    <li><a href="sobre.html">Sobre</a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <h1>Erro encontrado!</h1>
-            <p>Tente novamente ou contate a equipe de TI com os erros:</p>
-            <?php echo "Error: " . $sql . "<br>" . $conn->error; ?>
-            <button><a href="usuario.php"></a></button>
-        </main>
-    </body>
-    </html>
-<?php } ?>
+            if ($conn->query($sql) === TRUE) {?>
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" type="text/css" href="../css/style.css">
+                <title>Cadastro Confirmado</title>
+                <style>
+                    main {
+                        background-color: #76a490;
+                        width: 400px;
+                        height: 200px;
+                        text-align: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        border-radius: 20px;
+                    
+                    }
+                    button {
+                        background-color: #284b40;
+                        color: #fff;
+                        border-radius: 15px;
+                        width: 150px;
+                        height: 35px;
+                        margin-top: 8%;
+                        border: none;
+                    }
+                    button a{
+                        text-decoration: none;
+                        color: white;
+                    }
+                </style>
+            </head>
+            <body>
+                <header>
+                    <img class="logoclaro" src="../images/logoclaro.png" alt="Logo da empresa">
+                    <nav>
+                        <ul>
+                            <li><a href="../index.html">Home</a></li>
+                            <li><a href="plantas.php">Plantas</a></li>
+                            <li><a href="sensor.html">Sensores</a></li>
+                            <li><a href="calendario.html">Calendário</a></li>
+                            <li><a href="sobre.html">Sobre</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <h1>Usuário Cadastrado!</h1>
+                    <button><a href="../index.html">Voltar</a></button><br>
+                </main>
+            </body>
+            </html>
+            <?php } elseif ($conn->query($sql) === FALSE) { ?>
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" type="text/css" href="../css/style.css">
+                <title>Cadastro Confirmado</title>
+                <style>
+                    main {
+                        background-color: #76a490;
+                        width: 400px;
+                        height: 200px;
+                        text-align: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        border-radius: 20px;
+                    
+                    }
+                    button {
+                        background-color: #284b40;
+                        color: #fff;
+                        border-radius: 15px;
+                        width: 150px;
+                        height: 35px;
+                        margin-top: 8%;
+                        border: none;
+                    }
+                    button a{
+                        text-decoration: none;
+                        color: white;
+                    }
+                </style>
+            </head>
+            <body>
+                <header>
+                    <img class="logoclaro" src="../images/logoclaro.png" alt="Logo da empresa">
+                    <nav>
+                        <ul>
+                            <li><a href="../index.html">Home</a></li>
+                            <li><a href="plantas.php">Plantas</a></li>
+                            <li><a href="sensor.html">Sensores</a></li>
+                            <li><a href="calendario.html">Calendário</a></li>
+                            <li><a href="sobre.html">Sobre</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <h1>Erro encontrado!</h1>
+                    <p>Tente novamente ou contate a equipe de TI com os erros:</p>
+                    <?php echo "Error: " . $sql . "<br>" . $conn->error; ?>
+                    <button><a href="usuario.php">Voltar</a></button>
+                </main>
+            </body>
+            </html>
+            <?php } elseif ($result->num_rows > 0) { ?>
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" type="text/css" href="../css/style.css">
+                <title>Cadastro Confirmado</title>
+                <style>
+                    main {
+                        background-color: #76a490;
+                        width: 400px;
+                        height: 200px;
+                        text-align: center;
+                        margin-left: auto;
+                        margin-right: auto;
+                        border-radius: 20px;
+                    
+                    }
+                    button {
+                        background-color: #284b40;
+                        color: #fff;
+                        border-radius: 15px;
+                        width: 150px;
+                        height: 35px;
+                        margin-top: 8%;
+                        border: none;
+                    }
+                    button a{
+                        text-decoration: none;
+                        color: white;
+                    }
+                </style>
+            </head>
+            <body>
+                <header>
+                    <img class="logoclaro" src="../images/logoclaro.png" alt="Logo da empresa">
+                    <nav>
+                        <ul>
+                            <li><a href="../index.html">Home</a></li>
+                            <li><a href="plantas.php">Plantas</a></li>
+                            <li><a href="sensor.html">Sensores</a></li>
+                            <li><a href="calendario.html">Calendário</a></li>
+                            <li><a href="sobre.html">Sobre</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <h1>Este usuário já foi cadastrado!</h1>
+                    <button><a href="../index.html">Fazer login</a></button><br>
+                </main>
+            </body>
+            </html>
+            <?php }?>
